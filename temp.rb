efaf0047a5522e42feda7e9cd0b4812b9b26a452
Khalid T <khalid.tissire@gmail.com>
2020-01-23 01:06:20 +0000

    Done.

---
+++ temp.rb
@@ -0,0 +1,10 @@
+def speak_to_grandma(str)
+  uppercase = str
+  if (uppercase.toUpperCase() === uppercase)
+    return "YES INDEED!"
+  elsif (str === "I LOVE YOU GRANDMA!")
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
